9dfe0a971e6546ae21096df7f3634e5dac1a49a6
Parth Sachdeva <parth12062005@gmail.com>
2026-01-31 14:37:53 +0530

    acha

--- classification/methods/cocoop_batclip.py
+++ classification/methods/cocoop_batclip.py
@@ -126,9 +126,9 @@ class CoCoOpBATCLIP(TTAMethod):
                     m.running_var = None
         
         # Enable gradients for prompt learner parameters (excluding token_embedding)
-        for name, param in self.model.named_parameters():
-            if "prompt_learner" in name and "token_embedding" not in name:
-                param.requires_grad_(True)
+        # for name, param in self.model.named_parameters():
+        #     if "prompt_learner" in name and "token_embedding" not in name:
+        #         param.requires_grad_(True)
     
     def collect_params(self):
         """
@@ -147,7 +147,7 @@ class CoCoOpBATCLIP(TTAMethod):
                         params.append(p)
                         names.append(f"{nm}.{np}")
         
-        # Collect prompt learner parameters (excluding token_embedding)
+        Collect prompt learner parameters (excluding token_embedding)
         for name, param in self.model.named_parameters():
             if "prompt_learner" in name and "token_embedding" not in name and param.requires_grad:
                 params.append(param)
